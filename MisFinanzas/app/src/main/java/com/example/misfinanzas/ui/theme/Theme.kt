6991e697b3110da23659ff2a2b6fa69e6d2e7577
ZapatoProgramming <daniel.maimone03@gmail.com>
2025-04-22 15:40:59 -0600

    bug visual

--- MisFinanzas/app/src/main/java/com/example/misfinanzas/ui/theme/Theme.kt
+++ MisFinanzas/app/src/main/java/com/example/misfinanzas/ui/theme/Theme.kt
@@ -1,15 +1,11 @@
 package com.example.misfinanzas.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = Color(0xFF10B981),
@@ -18,7 +14,7 @@ private val DarkColorScheme = darkColorScheme(
     background = Color(0xFF192837),
     surface = Color(0xFF192837),
     onBackground = Color(0xFFEDE9D0),
-    onSurface = Color(0xFFEDE9D0),
+    onSurface = Color.Black,
     onPrimary = Color(0xFF192837),
 )
 
@@ -29,22 +25,16 @@ private val LightColorScheme = lightColorScheme(
     background = Color(0xFF192837),
     surface = Color(0xFF192837),
     onBackground = Color(0xFFEDE9D0),
-    onSurface = Color(0xFFEDE9D0),
+    onSurface = Color.Black,
     onPrimary = Color(0xFF192837),
 )
 
 @Composable
 fun MisFinanzasTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
